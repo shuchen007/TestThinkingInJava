bf4ab904f07f706cc1c0d152d2df259fd30eb2b7
sunhao <623784007@qq.com>
2018-05-10 22:40:36 +0800

    20180510hos

---
+++ src/com/hao/collection11/TestConCurrenHashmap.java
@@ -0,0 +1,21 @@
+package com.hao.collection11;
+
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Map;
+import java.util.Queue;
+import java.util.concurrent.ConcurrentHashMap;
+
+/**
+ * Created by Administrator on 2018/5/10.
+ */
+public class TestConCurrenHashmap {
+    Map map = new ConcurrentHashMap<>();
+
+    public static void main(String[] args) {
+        Queue queue = new LinkedList<>();
+        queue.add("df");
+
+    }
+}
+
