acb4e859ffad18fdb62c1a4a09a7ce0c0e2ca37a
sunhao <623784007@qq.com>
2018-06-14 13:10:00 +0800

    20180604com

---
+++ src/com/hao/TestPrintFileName.java
@@ -0,0 +1,29 @@
+package com.hao;
+
+import java.io.File;
+
+/**
+ * @Author sunhao
+ * @Description
+ * @Date 14:26 2018/6/13
+ * @Modified by
+ */
+
+public class TestPrintFileName {
+	private static void printLev(File dir,int level){
+		File[] subFiles = dir.listFiles();
+		for (File subFile : subFiles) {
+			for (int i = 0; i < level; i++) {
+				System.out.print("\t");
+			}
+			System.out.println(subFile);
+			if (subFile.isDirectory()) {
+				printLev(subFile,level++);
+			}
+		}
+	}
+	public static void main(String[] args) {
+		File dir = new File("F:/行政资料");
+		printLev(dir,0);
+	}
+}
